2897c095d5807b8d701d81513febba90a01ca059
Max Babich <max22dev@gmail.com>
2017-09-01 16:33:34 +0300

    fix load fastboot 1.0

--- index.js
+++ index.js
@@ -1,50 +1,54 @@
 /* jshint node: true */
 'use strict';
+const fastbootTransform = require('fastboot-transform');
 
 module.exports = {
-  name: 'ember-fullcalendar',
+    name: 'ember-fullcalendar',
 
-  // isDevelopingAddon: function() {
-  //   return true;
-  // },
+    // isDevelopingAddon: function() {
+    //   return true;
+    // },
 
-  options: {
-    nodeAssets: {
-      'fullcalendar': function() {
-        return {
-          enabled: !process.env.EMBER_CLI_FASTBOOT,
-          srcDir: 'dist',
-          import: ['fullcalendar.js', 'fullcalendar.css']
+    options: {
+        nodeAssets: {
+            'fullcalendar': {
+                import: {
+                    include: ['dist/fullcalendar.js', 'dist/fullcalendar.css'],
+                    processTree(input) {
+                        return fastbootTransform(input);
+                    }
+                }
+            },
+            'fullcalendar-scheduler': {
+                enabled: this.includeScheduler,
+                import: {
+                    include: ['dist/scheduler.js', 'dist/scheduler.css'],
+                    processTree(input) {
+                        return fastbootTransform(input);
+                    }
+                }
+            }
         }
-      },
-      'fullcalendar-scheduler': function() {
-        return {
-          enabled: !process.env.EMBER_CLI_FASTBOOT && this.includeScheduler,
-          srcDir: 'dist',
-          import: ['scheduler.js', 'scheduler.css']
+    },
+
+    included(app, parentAddon) {
+
+        var target = parentAddon || app;
+
+        // allow addon to be nested - see: https://github.com/ember-cli/ember-cli/issues/3718
+        if (target.app) {
+            target = target.app;
         }
-      }
+
+        var config = target.project.config(target.env) || {};
+
+        // Add scheduler to executable unless configured not to.
+        if (config.emberFullCalendar && config.emberFullCalendar.includeScheduler === true) {
+            this.includeScheduler = true;
+        } else {
+            this.includeScheduler = false;
+        }
+
+        this._super.included.apply(this, arguments);
     }
-  },
-
-  included: function(app, parentAddon) {
-
-    var target = parentAddon || app;
-
-    // allow addon to be nested - see: https://github.com/ember-cli/ember-cli/issues/3718
-    if (target.app) {
-      target = target.app;
-    }
-
-    var config = target.project.config(target.env) || {};
-
-    // Add scheduler to executable unless configured not to.
-    if (config.emberFullCalendar && config.emberFullCalendar.includeScheduler === true) {
-      this.includeScheduler = true;
-    } else {
-      this.includeScheduler = false;
-    }
-
-    this._super.included.apply(this, arguments);
-  }
 };
